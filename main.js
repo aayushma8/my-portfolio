5274429d089e640501c31f2cdef3906d5839c9b8
demo <aayushmab16@gmail.com>
2024-07-20 14:16:04 +0545

    Your commit message here

--- main.js
+++ main.js
@@ -7,12 +7,12 @@ const allProjects = document.getElementById("allProjects");
 const projectsArray = [
   
   {
-    image: "/projects/image.png",
+    image: "/projects/sessami.png",
     title: "Sessami Projects",
-    description: "This includes all the projects I did  during my traineeship at sessami.co",
+    description: "Here is a list of projects at Sessami.co.",
     techUsed: ["HTML", "CSS", "JS", "API"],
-    url: "https://aayushmabhusal8.github.io/Assignment1/",
-    githubUrl: "https://github.com/aayushmabhusal8/Assignment1",
+    url: "https://aayushma8.github.io/Assignment1/",
+    githubUrl: "https://github.com/aayushma8/Assignment1",
   },
   {
     image: "/projects/amazon.png",
